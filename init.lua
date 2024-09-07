1205863d483c6564c188e5a4d10def3de378f5bf
Seth Bashford <seth.bashford@proton.me>
2024-09-06 21:37:27 -0500

    upgrade lua_ls and update config

--- init.lua
+++ init.lua
@@ -286,35 +286,36 @@ require 'lspconfig'.cssls.setup {
 require 'lspconfig'.lua_ls.setup {
     on_attach = enable_lsp_keymaps_and_formatting,
     capabilities = capabilities,
-    cmd = { "/home/seth/installed/lua-language-server-3.6.23/bin/lua-language-server" },
+    cmd = { "/home/seth/installed/lua-language-server-3.10.5/bin/lua-language-server" },
     on_init = function(client)
         local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                    version = 'LuaJIT'
-                },
-                workspace = {
-                    library = { vim.env.VIMRUNTIME }
-                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                    -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-            })
-
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            return
         end
-        return true
-    end,
-    settings = {
-        Lua = {
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
             workspace = {
                 checkThirdParty = false,
-            },
-            telemetry = {
-                enable = false
+                library = {
+                    vim.env.VIMRUNTIME
+                    -- Depending on the usage, you might want to add additional paths here.
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                }
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                -- library = vim.api.nvim_get_runtime_file("", true)
             }
-        },
-    },
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
 }
 
 require 'nvim-treesitter.configs'.setup {
