8d0beb6686a7e040de459e6e5f8fd043415cd5cf
Seth Bashford <seth.bashford@proton.me>
2025-05-15 16:00:51 -0500

    probably a better way to accomplish this, but trying to enable formatting from biome while disabling ts_ls

--- init.lua
+++ init.lua
@@ -166,20 +166,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('my.lsp', {}),
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        -- Not sure how vim.lsp.completion works with or competes against blink.cmp...
-        --
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-        --if client:supports_method('textDocument/completion') then
-        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-        -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-        -- client.server_capabilities.completionProvider.triggerCharacters = chars
-        --vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        --end
-        --
-        -- Auto-format ("lint") on save.
-        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
+        if (not client:supports_method('textDocument/willSaveWaitUntil')
+                and client:supports_method('textDocument/formatting') and client.name ~= "ts_ls")
+            or client.name == "biome"
+        then
+            print('Enabling formatting for ' .. client.name)
             vim.api.nvim_create_autocmd('BufWritePre', {
                 group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
                 buffer = args.buf,
